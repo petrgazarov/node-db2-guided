34da8c3630dbd330f79970575779297cf2ef52e2
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-11 19:52:35 +0200

    wip

--- data/seeds/01-fruits.js
+++ data/seeds/01-fruits.js
@@ -1,11 +1,11 @@
 exports.seed = async function (knex) {
   // Truncates table - wipes it, resets primary key to 1
-  await knex('table_name').truncate()
+  await knex('fruits').truncate()
 
   // Inserts seed entries
-  await knex('table_name').insert([
-    { id: 1, colName: 'rowValue1' },
-    { id: 2, colName: 'rowValue2' },
-    { id: 3, colName: 'rowValue3' }
+  await knex('fruits').insert([
+    { fruit_name: 'pear' },
+    { fruit_name: 'apple' },
+    { fruit_name: 'pear' }
   ])
 }
