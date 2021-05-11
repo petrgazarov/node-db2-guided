91ae90edd6feee33b67f80575c5aae5561448954
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-11 19:14:25 +0200

    wip

--- data/migrations/20210511171058_make-fruits-table.js
+++ data/migrations/20210511171058_make-fruits-table.js
@@ -1,9 +1,7 @@
-
-
-exports.up = function() {
+exports.up = function(knex) {
   return knex.schema.createTable('fruits')
 };
 
 exports.down = function(knex) {
-
+  return knex.schema.dropTableIfExists('fruits')
 };
