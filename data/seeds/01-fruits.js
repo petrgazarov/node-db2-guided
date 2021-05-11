ad055c3d874d4f0fb98e4eafb74f88d506890b16
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-11 19:51:01 +0200

    wip

--- data/seeds/01-fruits.js
+++ data/seeds/01-fruits.js
@@ -1,12 +1,11 @@
-exports.seed = function (knex) {
+exports.seed = async function (knex) {
   // Truncates table - wipes it, resets primary key to 1
-  return knex('table_name').truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        { id: 1, colName: 'rowValue1' },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' }
-      ]);
-    });
-};
+  await knex('table_name').truncate()
+
+  // Inserts seed entries
+  await knex('table_name').insert([
+    { id: 1, colName: 'rowValue1' },
+    { id: 2, colName: 'rowValue2' },
+    { id: 3, colName: 'rowValue3' }
+  ])
+}
