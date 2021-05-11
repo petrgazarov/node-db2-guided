0b4845a6cfaf5fd98e69d6df87b89ef4d6397a05
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-11 19:54:11 +0200

    wip

--- data/seeds/01-fruits.js
+++ data/seeds/01-fruits.js
@@ -4,8 +4,8 @@ exports.seed = async function (knex) {
 
   // Inserts seed entries
   await knex('fruits').insert([
-    { fruit_name: 'pear', fruit_avg_weight: 12,  },
-    { fruit_name: 'apple', fruit_avg_weight: 4.6 },
+    { fruit_name: 'pear', fruit_avg_weight: 12, is_delicious: true },
+    { fruit_name: 'apple', fruit_avg_weight: 4.6, is_delicious: 0 },
     { fruit_name: 'orange', fruit_avg_weight: 1.4 }
   ])
 }
