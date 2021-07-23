de04fc00c1d746097455d44e22bc42864b34f514
Petr Gazarov <petrgazarov@gmail.com>
2021-07-22 18:50:56 -0700

    Add knex config file

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/produce.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+};
