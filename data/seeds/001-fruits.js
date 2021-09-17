7b15d7e2c5e4954c45bece981175247c7ce234ce
Petr Gazarov <petrgazarov@gmail.com>
2021-09-16 20:32:54 -0700

    Add color to fruits seed

--- data/seeds/001-fruits.js
+++ data/seeds/001-fruits.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
   return knex('fruits').truncate()
     .then(function () {
       return knex('fruits').insert([
-        { name: 'dragon fruit', avgWeightOz: 16, delicious: true },
-        { name: 'blueberry', avgWeightOz: 0.5, delicious: true },
-        { name: 'banana', avgWeightOz: 8, delicious: true },
+        { name: 'dragon fruit', avgWeightOz: 16, delicious: true, color: 'red' },
+        { name: 'blueberry', avgWeightOz: 0.5, delicious: true, color: 'blue' },
+        { name: 'banana', avgWeightOz: 8, delicious: true, color: 'yellow' },
       ]);
     });
 };
