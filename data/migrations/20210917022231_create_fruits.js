f88c53baea1a01a8fd10399ea6ffd6b4dd945aad
Petr Gazarov <petrgazarov@gmail.com>
2021-09-16 19:54:13 -0700

    Add create fruits migration

--- data/migrations/20210917022231_create_fruits.js
+++ data/migrations/20210917022231_create_fruits.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('fruits', table => {
+    table.increments();
+
+    table.string('name').unique().notNullable();
+    table.decimal('avgWeightOz');
+    table.boolean('delicious');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('fruits');
 };
